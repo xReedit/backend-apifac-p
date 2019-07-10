d9d7699caa8b45dd234c95e47fafe00586dc0871
Marcelo <xReedit@gmail.com>
2019-07-10 18:57:42 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+// var cors=require('cors');
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+const conn = mysql.createConnection({
+  host: '192.168.1.64',
+  user: 'resto',
+  password: '182182',
+  database: 'apifacturalo',
+  // multipleStatements: true, // acepta multimples consultas
+});
+ 
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Base de datos mysql, online...');
+});
+
+//show all products
+app.get('/api/documents',(req, res) => {
+    const arr = req.body;
+
+    let sql = `SELECT vd.id, d.id,DATE_FORMAT(d.date_of_issue, '%d/%m/%Y') as fecha, SUBSTRING_INDEX(dt.description, ' ', 1) as tipo_doc
+            , concat(d.series, LPAD(d.number,7, '0')) as num_doc, cast(d.customer->>'$.name' as char(250)) as nom_cliente, cast(d.customer->>'$.number' as char(11)) as ruc
+            , format(d.total_value,2) as subtotal, format((d.total_value-total_exonerated),2) as igv, format(d.total,2) as total, (if (vd.id, 'ANULADO', 'ACEPTADO')) as estado
+        from documents as d
+            left join voided_details as vd on d.id=vd.document_id
+            inner join document_types as dt on d.document_type_id=dt.id
+        where d.user_id=${arr.id} and (MONTH(d.date_of_issue)=${arr.m} and YEAR(d.date_of_issue)=${arr.y})
+        order by d.id desc`;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json({"status": 200, "error": null, "data": results});
+    });
+});
+
+app.get('/api/companies',(req, res) => {
+    const arr = req.body;
+
+    let sql = `SELECT user_id, number as ruc, name as razonsocial, trade_name as razoncomercial from companies`;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json({"status": 200, "error": null, "data": results});
+    });
+});
+
+//Server listening
+app.listen(3000,() =>{
+  console.log('Server started on port 3000...');
+});
